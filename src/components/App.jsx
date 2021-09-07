7868643eb31192d05ff3c0826a5dacab34606ee2
Macm91 <macm91.mc@gmail.com>
2021-09-07 18:03:09 -0400

    completed navigation functionality for going to the next and previous books.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,22 +15,49 @@ class App extends Component {
         };       
     }
 
+    goToNextBook = () => {
+        debugger;
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber : tempBookNumber
+        })
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--; 
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber : tempBookNumber
+        })
+    }
+
+
     render(){
         return(
           <div className="container-fluid">
               <TitleBar/>
               <div className="row">
-                <div className="col=md=4"></div>
+                <div className="col-md-4">
                 {/* button will be here to move to the previous book viewed */}
-                <div className="col=md=4">
-                    {/* display book with cover here */}
+                    <button onClick={this.goToPreviousBook}>Previous Book</button>
+                </div>
+                <div className="col-md-4">
+                    {/* display book with cover here */} 
                     <h1> {this.books[this.state.bookNumber].title} </h1>
                     <h4> {this.books[this.state.bookNumber].author} </h4>
                 </div>
-                <div className="col=md=4">
+                <div className="col-md-4">
                     {/* Button here to move to next book */}
-                </div>
+                    <button onClick={this.goToNextBook}>Next Book</button>
               </div>
+          </div> 
           </div> 
         )
     }
