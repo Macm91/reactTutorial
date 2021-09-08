3bb15982d777b38c9a581faf518bca5c556bb939
Macm91 <macm91.mc@gmail.com>
2021-09-08 09:07:47 -0400

    created a separate component for the footer .

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+
+const Footer = ()=>{
+    return(
+        <div className="footer">
+            <h6 className="footerText"> @2021 </h6>
+        </div>
+    )
+}
+
+export default Footer;
